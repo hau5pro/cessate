2719c8a4a96070e6a7b812e6706deca7ac0d59d0
House Yokeswaran <h4u5d3v@gmail.com>
2025-04-27 11:39:55 -0400

    fix deploy

--- vite-node-polyfill.ts
+++
@@ -1,9 +0,0 @@
-// vite-node-polyfill.ts
-
-import { randomFillSync } from "crypto";
-
-if (typeof (globalThis as any).crypto === "undefined") {
-  (globalThis as any).crypto = {
-    getRandomValues: (arr: any) => randomFillSync(arr),
-  };
-}
