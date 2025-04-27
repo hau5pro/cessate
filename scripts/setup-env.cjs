09111a69fa282d1607735aa274ce9d471518206d
House Yokeswaran <h4u5d3v@gmail.com>
2025-04-27 11:43:35 -0400

    another fix attempt

--- scripts/setup-env.cjs
+++
@@ -1,8 +0,0 @@
-// scripts/setup-env.cjs
-
-if (typeof globalThis.crypto === "undefined") {
-  const { randomFillSync } = require("crypto");
-  globalThis.crypto = {
-    getRandomValues: (arr) => randomFillSync(arr),
-  };
-}
