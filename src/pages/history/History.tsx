ea2a2760a178399362b1bf91f2552de8715b22cd
House Yokeswaran <h4u5d3v@gmail.com>
2025-05-06 15:42:40 -0400

    some updates to empty pages

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,5 +1,15 @@
+import { Box, Typography } from '@mui/material';
+
+import styles from './History.module.css';
+
 function HistoryPage() {
-  return <div>History</div>;
+  return (
+    <Box className={styles.HistoryContainer}>
+      <Typography className={styles.HistoryHeader} variant="h2">
+        History
+      </Typography>
+    </Box>
+  );
 }
 
 export default HistoryPage;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,17 @@
+import { Box, Typography } from '@mui/material';
+
+import styles from './Home.module.css';
+import { useUserSettingsStore } from '@store/useUserSettingsStore';
+
 function HomePage() {
-  return <div>Home</div>;
+  const settings = useUserSettingsStore((state) => state.settings);
+  return (
+    <Box className={styles.HomeContainer}>
+      <Typography variant="h2" textAlign={'center'}>
+        {settings?.name ? `Welcome back, ${settings.name}!` : 'Welcome back!'}
+      </Typography>
+    </Box>
+  );
 }
 
 export default HomePage;
