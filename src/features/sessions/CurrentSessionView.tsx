a75189d7dbf6742e13050c2fd7067d5e5fde4dfc
House Yokeswaran <h4u5d3v@gmail.com>
2025-05-08 22:27:53 -0400

    current session animation

--- src/features/sessions/CurrentSessionView.tsx
+++ src/features/sessions/CurrentSessionView.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { Box, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 
@@ -11,10 +12,20 @@ import { useSessionStore } from '@store/useSessionStore';
 
 dayjs.extend(duration);
 
+const slideDownVariants = {
+  hidden: { opacity: 0, y: -20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5, ease: 'easeOut' },
+  },
+  exit: { opacity: 0, y: -20, transition: { duration: 0.3 } },
+};
+
 function CurrentSessionView() {
   const session = useSessionStore((state) => state.currentSession);
-
   const [now, setNow] = useState(dayjs());
+
   useEffect(() => {
     const interval = setInterval(() => setNow(dayjs()), 1000);
     return () => clearInterval(interval);
@@ -33,54 +44,68 @@ function CurrentSessionView() {
   const normalizedPercent = percent / 100;
 
   return (
-    <Box
-      textAlign="center"
-      p={1}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        gap: 0.5,
-        flexGrow: 1,
-      }}
-    >
-      <Typography variant="h3" color="secondary" mt={1}>
-        You've abstained for
-      </Typography>
-      <Typography
-        variant="h2"
-        color={theme.palette.common.white}
-        sx={{ fontSize: '3rem' }}
+    <AnimatePresence>
+      <motion.div
+        key="current-session"
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        variants={slideDownVariants}
       >
-        {timePassed.days()}d {timePassed.hours()}h {timePassed.minutes()}m
-      </Typography>
-
-      <Typography variant="body1" color="secondary" mt={2}>
-        Time remaining
-      </Typography>
-      <Typography variant="subtitle1">{timeLeft.format('HH:mm:ss')}</Typography>
-
-      <Box
-        textAlign="center"
-        p={2}
-        sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}
-      >
-        <Progress
-          percent={percent}
-          width={'60%'}
-          animate={true}
-          sx={{ alignSelf: 'center' }}
-        />
-        <Typography variant="body2" display="block" mt={1}>
-          <span
-            style={{ color: ColorUtils.interpolateColor(normalizedPercent) }}
+        <Box
+          textAlign="center"
+          p={1}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            gap: 0.5,
+            flexGrow: 1,
+          }}
+        >
+          <Typography variant="h3" color="secondary" mt={1}>
+            You've abstained for
+          </Typography>
+          <Typography
+            variant="h2"
+            color={theme.palette.common.white}
+            sx={{ fontSize: '3rem' }}
           >
-            {Math.floor(percent)}%
-          </span>{' '}
-          of your target completed{timeLeft.asSeconds() === 0 ? '!' : ''}
-        </Typography>
-      </Box>
-    </Box>
+            {timePassed.days()}d {timePassed.hours()}h {timePassed.minutes()}m
+          </Typography>
+
+          <Typography variant="body1" color="secondary" mt={2}>
+            Time remaining
+          </Typography>
+          <Typography variant="subtitle1">
+            {timeLeft.format('HH:mm:ss')}
+          </Typography>
+
+          <Box
+            textAlign="center"
+            p={2}
+            sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}
+          >
+            <Progress
+              percent={percent}
+              width={'60%'}
+              animate={true}
+              sx={{ alignSelf: 'center' }}
+            />
+            <Typography variant="body2" display="block" mt={1}>
+              <span
+                style={{
+                  color: ColorUtils.interpolateColor(normalizedPercent),
+                }}
+              >
+                {Math.floor(percent)}%
+              </span>{' '}
+              of your target completed{timeLeft.asSeconds() === 0 ? '!' : ''}
+            </Typography>
+          </Box>
+        </Box>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
