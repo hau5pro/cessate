3c9ab789491869d1a06887fc8d8f9f8b10356852
House Yokeswaran <h4u5d3v@gmail.com>
2025-05-08 22:24:56 -0400

    login animations

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,27 +1,48 @@
 import BaseButton from '@components/BaseButton';
+import Box from '@mui/material/Box';
 import { GoogleIcon } from '@components/CustomIcons';
+import { motion } from 'framer-motion';
 import { signInWithGoogle } from '@services/authService';
+
+const containerVariants = {
+  hidden: { scale: 0.9, y: -50, opacity: 0 },
+  visible: {
+    scale: 1,
+    y: 0,
+    opacity: 1,
+    transition: { type: 'spring', stiffness: 400, damping: 15 },
+  },
+};
 
 function LoginPage() {
   return (
-    <div
-      style={{
-        flexGrow: 1,
+    <Box
+      sx={{
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'end',
+        justifyContent: 'flex-end',
+        overflow: 'hidden',
+        flexGrow: 1,
         padding: '1rem',
         height: '100%',
       }}
     >
-      <BaseButton
-        startIcon={<GoogleIcon />}
-        sx={{ padding: '1rem' }}
-        onClick={signInWithGoogle}
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
       >
-        Sign In With Google
-      </BaseButton>
-    </div>
+        <BaseButton
+          startIcon={<GoogleIcon />}
+          sx={{ padding: '1rem' }}
+          onClick={signInWithGoogle}
+        >
+          Sign In With Google
+        </BaseButton>
+      </motion.div>
+    </Box>
   );
 }
 
