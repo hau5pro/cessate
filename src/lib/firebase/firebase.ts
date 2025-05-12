01b602a394e2982ff8ebc9afe42284fee88633cc
House Yokeswaran <h4u5d3v@gmail.com>
2025-05-11 22:36:41 -0400

    debug pipeline

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -10,6 +10,7 @@ import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 
 import { initializeApp } from 'firebase/app';
 
+console.log(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
