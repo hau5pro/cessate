74e80b7333e5f49f9d8fd1887526fcd7a41f6874
House Yokeswaran <h4u5d3v@gmail.com>
2025-05-11 22:55:14 -0400

    trigger build

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -10,7 +10,6 @@ import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 
 import { initializeApp } from 'firebase/app';
 
-console.log(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
