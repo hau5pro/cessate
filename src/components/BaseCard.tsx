4376d10b62520c2dac71bb23056c6db704610ca6
House Yokeswaran <h4u5d3v@gmail.com>
2025-05-12 08:25:36 -0400

    mobile styles

--- src/components/BaseCard.tsx
+++ src/components/BaseCard.tsx
@@ -6,13 +6,13 @@ const BaseCard = ({ children, sx, ...props }: CardProps) => {
       {...props}
       sx={{
         background: 'rgba(17, 25, 40, 0.25)',
-        borderRadius: 12,
-        border: '1px solid rgba(255, 255, 255, 0.3)',
-        boxShadow: '0 4px 30px rgba(0, 0, 0, 0.2)',
+        borderRadius: { sm: 12 },
+        border: { sm: '1px solid rgba(255, 255, 255, 0.3)' },
+        boxShadow: { sm: '0 4px 30px rgba(0, 0, 0, 0.2)' },
         backdropFilter: 'blur(25px) saturate(180%)',
         WebkitBackdropFilter: 'blur(25px) saturate(180%)',
         color: 'white',
-        padding: 2,
+        padding: { xs: 1, sm: 2 },
         ...sx,
       }}
     >
